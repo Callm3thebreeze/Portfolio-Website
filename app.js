7105e74b53c3e3da8d4bff214e8f9f7a2050a9b9
callm3thebreeze <jesusvillarjimenez@gmail.com>
2022-05-27 20:44:49 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelector('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSelections = document.querySelector('main-content');
