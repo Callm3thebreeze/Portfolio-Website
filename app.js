b5043cd69e52fa064d5a13807c4ace440ba284bc
callm3thebreeze <jesusvillarjimenez@gmail.com>
2022-05-31 00:22:12 +0200

    Home section

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSelections = document.querySelector('main-content');
+const allSections = document.querySelector('.main-content');
 
 
 function pageTransitions(){
@@ -13,6 +13,25 @@ function pageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    //Sections active
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if (id) {
+            //remove active from other buttons
+            sectBtn.forEach((btn)=>{
+                btn.classList.remove('active');
+            })
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+            
+        }
+    })
 }
 
 pageTransitions();
